59e3baacfb112c50b0edd65b00aa58f266e21603
hsolbrig <solbrig@jhu.edu>
2020-04-08 15:34:45 -0500

    Add automatic downloader

---
+++ data/download.sh
@@ -0,0 +1,10 @@
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_ICD9CM_PX_2018AA.zip
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_CPT_PX_2018AA.zip​
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_HCPCS_PX_2018AA.zip​
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_ICD9CM_DX_2018AA.zip​
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_ICD9CM_PX_2018AA.zip​
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_ICD10CM_DX_2018AA.zip​
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_ICD10PCS_PX_2018AA.zip​
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_LOINC_LAB_2018AA.zip​
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_MED_VA_V2_092818.zip​
+curl -O https://ncatswiki.dbmi.pitt.edu/acts/raw-attachment/wiki/ACT%20Ontology%20Version%202.0.1/ACT_MED_ALPHA_V2_121318.zip​
